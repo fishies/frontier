12b81e82738fa02e1f16710d7445fa674022e6a6
sowrd299 <ryan.ward46@gmail.com>
2017-04-22 14:31:23 -0700

    Early work on Input manager

---
+++ Assets/Scripts/UI/InputManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+/* A script to act as a common interface for all I/O devices interacting with the game
+ */
+
+
+public class InputManager : MonoBehaviour {
+
+    public enum Modes { SELECT, PLACE_ROAD, PLACE_RAIDER, PLACE_TOWER };
+
+    private Modes mode; //stores the current input mode
+
+    public Modes Mode {
+        get {
+            return mode;
+        }
+        set {
+            Modes old_mode = mode;
+            mode = value;
+        }
+    }
+
+    private void update_mode() {
+        /* To be called when the mode changes,
+         * applies associated changes
+         */ 
+          
+    } 
+
+}
---
+++ Assets/Scripts/UI/KeyboardManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class KeyboardManager : MonoBehaviour {
+
+    private InputManager im; //store the associated input manager;
+
+    void Start() {
+        im = GetComponent<InputManager>();
+    }
+
+    void Update() {
+
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Alpha1)) {
+            im.Mode = InputManager.Modes.SELECT;
+        } else if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            im.Mode = InputManager.Modes.PLACE_ROAD;
+        } else if (Input.GetKeyDown(KeyCode.Alpha3)) {
+            im.Mode = InputManager.Modes.PLACE_RAIDER;
+        } else if (Input.GetKeyDown(KeyCode.Alpha4)) {
+            im.Mode = InputManager.Modes.PLACE_TOWER;
+        }
+        //TODO: insert WASD pan, change turn
+
+    }
+
+}
