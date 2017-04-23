af4f5a6641af1fb1c1b9bb1a0344fba16d96d06b
tolkwes <tolkwes@gmail.com>
2017-04-22 17:31:29 -0700

    Road Handler
    Creates LineRenderer between two points of interest on map and can
    return those points of interests.

---
+++ Assets/Scripts/RoadHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadHandler : MonoBehaviour {
+    public GameObject point1;
+    public GameObject point2;
+
+	public void setEndpoints(Transform point1, Transform point2) {
+        Vector3[] arg = new Vector3[] { point1.position, point2.position };
+        gameObject.GetComponent<LineRenderer>().SetPositions(arg);
+    }
+
+    public GameObject[] getEndpoints() {
+        return (new GameObject[] {point1, point2});
+    }
+}
