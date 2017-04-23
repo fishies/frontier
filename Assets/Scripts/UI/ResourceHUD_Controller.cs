2c2ec017bb3c4451ef4e381df2b14d5746aa6720
sowrd299 <ryan.ward46@gmail.com>
2017-04-23 00:05:29 -0700

    Complete HUD

---
+++ Assets/Scripts/UI/ResourceHUD_Controller.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceHUD_Controller : MonoBehaviour {
+
+    public int Player;
+    private PlayerInfo pi;
+    private Text[] disps;
+
+    void Start() {
+        //init the PlayerInfo
+        foreach (PlayerInfo p in FindObjectsOfType<PlayerInfo>()) {
+            if (p.playerID == Player) {
+                pi = p;
+                break;
+            }
+        }
+        //init the text displays
+        disps = new Text[4];
+        initText(Production.Resource.Food, "Food");
+        initText(Production.Resource.Steel, "Iron");
+        initText(Production.Resource.Cement, "Stone");
+        initText(Production.Resource.Lumber, "Lumber");
+    }
+
+    private void initText(Production.Resource r, string s) {
+        disps[(int)r] = transform.FindChild(s).GetComponentInChildren<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //update the texts to reflect the stockpile contents;
+	    for(int i = 0; i < disps.Length; i++) {
+            disps[i].text = pi.stockpile[i].ToString();
+        }
+	}
+}
---
+++ Assets/Scripts/UI/TurnDispController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TurnDispController : MonoBehaviour {
+
+    private GameManager gm;
+    private Text t;
+    
+	void Start () {
+        gm = FindObjectOfType<GameManager>();
+        t = GetComponentInChildren<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        t.text = "Player " + gm.currentPlayer.ToString();	
+	}
+}
