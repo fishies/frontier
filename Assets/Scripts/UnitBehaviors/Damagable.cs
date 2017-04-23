17846734995b86a569b8b59853c4e97d7b5e0dba
fishies <andrewx2@uci.edu>
2017-04-23 14:49:06 -0700

    Updated Raider and Tower prefabs. Damagable.cs should be modifying HealthText

--- Assets/Scripts/UnitBehaviors/Damagable.cs
+++ Assets/Scripts/UnitBehaviors/Damagable.cs
@@ -2,22 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Damagable : MonoBehaviour {
-    public int health;
-	
-	void Update() {
-        if(health <= 0) {
-            Destroy(gameObject);
-        }
+public class Damagable : MonoBehaviour
+{
+	public int health;
+	TextMesh text;
+
+	void Awake ()
+	{
+		Production p = gameObject.GetComponent<Production> ();
+		text = gameObject.GetComponentInChildren<TextMesh> ();
+		text.color = Color.black;
+		if (p != null) {
+			// text.color [(p.ownerID % 3)] = 1.0f;
+			// Cannot modify a value type return value of 'UnityEngine.TextMesh.color'??????
+
+			switch (p.ownerID % 5) {
+			case 0:
+				text.color = Color.red;
+				break;
+			case 1:
+				text.color = Color.green;
+				break;
+			case 2:
+				text.color = Color.blue;
+				break;
+			case 3:
+				text.color = Color.yellow;
+				break;
+			case 4:
+				text.color = Color.magenta;
+				break;
+			default:
+				break;
+			}
+		}
 	}
 
-    public void takeDamage(int attackStrength) {
-        if (tag == "Capital") {
-            GetComponent<PlayerInfo>().stockpile[(int)Production.Resource.Food] -= attackStrength;
-        }
-        else
-        {
-            health = health - attackStrength;
-        }
-    }
+	void Update ()
+	{
+		if (health <= 0) {
+			Destroy (gameObject);
+		}
+	}
+
+	public void takeDamage (int attackStrength)
+	{
+		if (tag == "Capital") {
+			GetComponent<PlayerInfo> ().stockpile [(int)Production.Resource.Food] -= attackStrength;
+		} else {
+			health -= attackStrength;
+			text.text = "";
+			for (int i = 0; i < health; ++i) {
+				text.text += "♥";
+			}
+		}
+	}
 }
