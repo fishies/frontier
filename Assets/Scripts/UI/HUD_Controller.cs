e03304d9fe29d60721a2ff97682f93104f2ea082
sowrd299 <ryan.ward46@gmail.com>
2017-04-22 22:02:59 -0700

    Tool Bar

---
+++ Assets/Scripts/UI/HUD_Controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUD_Controller : MonoBehaviour {
+
+    InputManager im;
+
+    void Start() {
+        im = FindObjectOfType<InputManager>();
+    }
+
+    public void SetMode(int mode) {
+        //need to take int and cast so works with Unity
+        im.Mode = (InputManager.Modes)mode;
+    }
+
+    public void NextTurn() {
+        im.NextTurn();
+    }
+
+}
