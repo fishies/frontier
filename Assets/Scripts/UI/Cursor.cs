01dd61d2b821aeb19113f14f45cefb868910ba42
tolkwes <tolkwes@gmail.com>
2017-04-23 14:36:09 -0700

    HUD Cursor added
    HUD now has cursor that shows current item selected.

---
+++ Assets/Scripts/UI/Cursor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour {
+    InputManager im;
+    GameObject[] itemArray;
+
+    void Start() {
+        im = FindObjectOfType<InputManager>();
+        itemArray = new GameObject[4] {GameObject.Find("Cursor"),
+                                                GameObject.Find("Road"),
+                                                GameObject.Find("Raider"),
+                                                GameObject.Find("Tower")};
+    }
+
+    public void ChangeActiveItem(int currentItem) {
+        Vector3 currentItemPosition = itemArray[currentItem].transform.position;
+        currentItemPosition.x += 20;
+        currentItemPosition.y -= 20;
+        gameObject.transform.position = currentItemPosition;
+    }
+}
