1c51b8c68a7377917449a8d62ad8cfc95ff12fe9
sowrd299 <ryan.ward46@gmail.com>
2017-04-24 00:10:36 -0700

    for posterities sake

--- Assets/Scripts/GameInit/BetterGameInit.cs
+++ Assets/Scripts/GameInit/BetterGameInit.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class BetterGameInit : MonoBehaviour {
 
+    public Sprite farmImg;
+    public Sprite quarryImg;
+    public Sprite lumberImg;
+    public Sprite ironImg;
+
+
 	public GameObject villagePrefab; // U MUST SET THIS THX
 	public GameObject capitalPrefab; // USE THE EDITOR THX
 
@@ -16,165 +22,200 @@ public class BetterGameInit : MonoBehaviour {
 
 	void Awake() {generateBoard ();}
 
-	public void generateBoard () {
-		Vector2 boardSize = new Vector2 ((-topLeft.x) - topLeft.x, topLeft.y - (-topLeft.y));
-		Vector2 cellSize = new Vector2 (boardSize.x / boardX, boardSize.y / boardY);
+    public void generateBoard() {
+        Vector2 boardSize = new Vector2((-topLeft.x) - topLeft.x, topLeft.y - (-topLeft.y));
+        Vector2 cellSize = new Vector2(boardSize.x / boardX, boardSize.y / boardY);
 
-		List<Vector2> positionWithinCell = new List<Vector2>();
-		for (int i = 0; i < halfSize; ++i) {
-			positionWithinCell.Add(new Vector2(Random.value * cellSize.x, Random.value * cellSize.y));
-		}
-		positionWithinCell [27] = new Vector2 (0.5f * cellSize.x, 0.5f * cellSize.y);
+        List<Vector2> positionWithinCell = new List<Vector2>();
+        for (int i = 0; i < halfSize; ++i) {
+            positionWithinCell.Add(new Vector2(Random.value * cellSize.x, Random.value * cellSize.y));
+        }
+        positionWithinCell[27] = new Vector2(0.5f * cellSize.x, 0.5f * cellSize.y);
 
-		List<int> villageSize = new List<int>();
-		List<Production.Resource> villageType = new List<Production.Resource>();
+        List<int> villageSize = new List<int>();
+        List<Production.Resource> villageType = new List<Production.Resource>();
 
-		// generate non-farm villages
-		List<int> sizeBucket = new List<int>{3,2,2,2,1,1,1,1,1}; // bucket of sizes to be placed
-		List<Production.Resource> typeBucket = new List<Production.Resource>
-		{Production.Resource.Cement,Production.Resource.Lumber,Production.Resource.Steel}; // bucket of types to be placed: this will be refilled in the loop when empty
+        // generate non-farm villages
+        List<int> sizeBucket = new List<int> { 3, 2, 2, 2, 1, 1, 1, 1, 1 }; // bucket of sizes to be placed
+        List<Production.Resource> typeBucket = new List<Production.Resource>
+        {Production.Resource.Cement,Production.Resource.Lumber,Production.Resource.Steel}; // bucket of types to be placed: this will be refilled in the loop when empty
 
-		for (int i = 0; i < halfSize; ++i) {
-			int currSize = 0;
-			Production.Resource currType = Production.Resource.Food; // i really should define Production.Resource.None == -1 tbh
-			if (i != 27) { // branchpredictor-san, gomen...
-				if (!(i == 28 && villageSize[20] > 0)) { // i really hope the compiler does manual loop unrolling lol
-					// probability of placing something should depend heavily on remaining elements in bucket
-					// it should also approach 1 as i approaches halfSize
-					if (Random.value > ((1.0f * (i + 0.125f) * sizeBucket.Count) / (halfSize - 1.625f))) {
-						int indexHit;
+        for (int i = 0; i < halfSize; ++i) {
+            int currSize = 0;
+            Production.Resource currType = Production.Resource.Food; // i really should define Production.Resource.None == -1 tbh
+            if (i != 27) { // branchpredictor-san, gomen...
+                if (!(i == 28 && villageSize[20] > 0)) { // i really hope the compiler does manual loop unrolling lol
+                                                         // probability of placing something should depend heavily on remaining elements in bucket
+                                                         // it should also approach 1 as i approaches halfSize
+                    if (Random.value > ((1.0f * (i + 0.125f) * sizeBucket.Count) / (halfSize - 1.625f))) {
+                        int indexHit;
 
-						if (sizeBucket.Count > 0) {
-							indexHit = Random.Range (0, sizeBucket.Count);
-							currSize = sizeBucket [indexHit];
-							sizeBucket.RemoveAt (indexHit);
-						}
 
-						if (typeBucket.Count < 1) { // refill the bucket!
-							typeBucket.Add (Production.Resource.Cement);
-							typeBucket.Add (Production.Resource.Lumber);
-							typeBucket.Add (Production.Resource.Steel);
-						}
+                        if (sizeBucket.Count > 0) {
+                            indexHit = Random.Range(0, sizeBucket.Count);
+                            currSize = sizeBucket[indexHit];
+                            sizeBucket.RemoveAt(indexHit);
+                        }
 
-						indexHit = Random.Range (0, typeBucket.Count);
-						currType = typeBucket [indexHit];
-						typeBucket.RemoveAt (indexHit);
-					}
-				}
-			}
-			villageSize.Add (currSize);
-			villageType.Add (currType);
-		}
+                        if (typeBucket.Count < 1) { // refill the bucket!
+                            typeBucket.Add(Production.Resource.Cement);
+                            typeBucket.Add(Production.Resource.Lumber);
+                            typeBucket.Add(Production.Resource.Steel);
+                        }
 
-		// Debug.Assert (sizeBucket.Count == 0);
-		sizeBucket.Add (2);
-		sizeBucket.Add (1);
-		sizeBucket.Add (1);
-		HashSet<int> innerIndices = new HashSet<int>{27,28,29,20,21,14}; // game jam stratz
+                        indexHit = Random.Range(0, typeBucket.Count);
+                        currType = typeBucket[indexHit];
+                        typeBucket.RemoveAt(indexHit);
+                    }
+                }
 
-		// place outer farms
-		for (int i = 0; sizeBucket.Count > 0; i = (i+1) % halfSize) {
-			if (villageSize[i] > 0 || innerIndices.Contains(i))
-				continue;
-			if (Random.value > 0.875f) {
-				int indexHit = Random.Range (0, sizeBucket.Count);
-				villageSize [i] = sizeBucket [indexHit];
-				sizeBucket.RemoveAt (indexHit);
-			}
-		}
+            }
+            villageSize.Add(currSize);
+            villageType.Add(currType);
+        }
 
-		// place starting farm: the only big farm as well as the only "inner" farm
-		if (villageSize [20] > 0) {
-			villageSize [28] = 3;
-		} else if (villageSize [28] > 0) {
-			villageSize [20] = 3;
-		} else {
-			villageSize [(Random.value > 0.5f) ? 20 : 28] = 3;
-		}
+        // Debug.Assert (sizeBucket.Count == 0);
+        sizeBucket.Add(2);
+        sizeBucket.Add(1);
+        sizeBucket.Add(1);
+        HashSet<int> innerIndices = new HashSet<int> { 27, 28, 29, 20, 21, 14 }; // game jam stratz
 
-		// actual instantiation step
-		for (int row = 0, i = 0; row < boardY; ++row) {
-			for (int col = 0; col < row + 2; ++col, ++i) {
-				if (villageSize[i] > 0) {
-				//do math with row/col num to get screen coords
-					Vector3 spawnLoc = Vector3.Reflect(
-						new Vector3 (cellSize.x * col + positionWithinCell[i].x - Mathf.Abs(topLeft.x),
-						(cellSize.y * row + positionWithinCell[i].y - Mathf.Abs(topLeft.y)),0.0f),
-						Vector3.up);
-				//instantiate based on production type
-					GameObject village = Instantiate(villagePrefab, spawnLoc, Quaternion.identity);
-					Production p = village.GetComponent<Production> ();
-					p.ownerID = 1;
-					// i need to set: village income, sprite, and text number indicating size
-					switch (villageType[i]) {
-					case Production.Resource.Food:
-						p.income [(int)Production.Resource.Food] = 2 + villageSize [i];
-						// TODO: set sprite and text number :^) for each of deez
-						break;
-					case Production.Resource.Cement:
-						p.income [(int)Production.Resource.Food] = - villageSize [i];
-						p.income [(int)Production.Resource.Cement] = villageSize [i];
-						break;
-					case Production.Resource.Lumber:
-						p.income [(int)Production.Resource.Food] = - villageSize [i];
-						p.income [(int)Production.Resource.Cement] = 1 + villageSize [i];
-						break;
-					case Production.Resource.Steel:
-						p.income [(int)Production.Resource.Food] = - villageSize [i];
-						p.income [(int)Production.Resource.Cement] = 1 + villageSize [i];
-						break;
-					default:
-						break;
-					}
-				}
-			}
-		}
+        // place outer farms
+        for (int i = 0; sizeBucket.Count > 0; i = (i + 1) % halfSize) {
+            if (villageSize[i] > 0 || innerIndices.Contains(i))
+                continue;
+            if (Random.value > 0.875f) {
+                int indexHit = Random.Range(0, sizeBucket.Count);
+                villageSize[i] = sizeBucket[indexHit];
+                sizeBucket.RemoveAt(indexHit);
+            }
+        }
 
-		// mirror mirror on the wall, which kingdom be the edgiest of them all?
-		//positionWithinCell = reversedList (positionWithinCell);
-		//villageSize = reversedList (villageSize);
-		//villageType = reversedList (villageType);
+        // place starting farm: the only big farm as well as the only "inner" farm
+        int fIndex = 0;
+        if (villageSize[20] > 0) {
+            villageSize[28] = 3;
+            fIndex = 28;
+        } else if (villageSize[28] > 0) {
+            villageSize[20] = 3;
+            fIndex = 20;
+        } else {
+            fIndex = (Random.value > 0.5f) ? 20 : 28;
+            villageSize[fIndex] = 3;
+        }
 
-		// yes i literally copypasted but i gotta change some shit too
-		for (int row = 0, i = halfSize-1; row < boardY; ++row) {
-			for (int col = row + 2; col < boardX; ++col, --i) {
-				if (villageSize[i] > 0) {
-					//do math with row/col num to get screen coords
-					Vector3 spawnLoc = Vector3.Reflect(
-						new Vector3 (cellSize.x * col - positionWithinCell[i].x - Mathf.Abs(topLeft.x),
-							(cellSize.y * row - positionWithinCell[i].y - Mathf.Abs(topLeft.y)),0.0f),
-						Vector3.up);
-					//instantiate based on production type
-					GameObject village = Instantiate(villagePrefab, spawnLoc, Quaternion.identity);
-					Production p = village.GetComponent<Production> ();
-					p.ownerID = 2;
-					// i need to set: village income, sprite, and text number indicating size
-					switch (villageType[i]) {
-					case Production.Resource.Food:
-						p.income [(int)Production.Resource.Food] = 2 + villageSize [i];
-						// TODO: set sprite and text number :^) for each of deez
-						break;
-					case Production.Resource.Cement:
-						p.income [(int)Production.Resource.Food] = - villageSize [i];
-						p.income [(int)Production.Resource.Cement] = villageSize [i];
-						break;
-					case Production.Resource.Lumber:
-						p.income [(int)Production.Resource.Food] = - villageSize [i];
-						p.income [(int)Production.Resource.Cement] = 1 + villageSize [i];
-						break;
-					case Production.Resource.Steel:
-						p.income [(int)Production.Resource.Food] = - villageSize [i];
-						p.income [(int)Production.Resource.Cement] = 1 + villageSize [i];
-						break;
-					default:
-						break;
-					}
-				}
-			}
-		}
+        // actual instantiation step
+        for (int row = 0, i = 0; row < boardY; ++row) {
+            for (int col = 0; col < row + 2; ++col, ++i) {
+                if (villageSize[i] > 0) {
+                    //do math with row/col num to get screen coords
+                    Vector3 spawnLoc = Vector3.Reflect(
+                        new Vector3(cellSize.x * col + positionWithinCell[i].x - Mathf.Abs(topLeft.x),
+                        (cellSize.y * row + positionWithinCell[i].y - Mathf.Abs(topLeft.y)), 0.0f),
+                        Vector3.up);
+                    //instantiate based on production type
+                    GameObject village = Instantiate(villagePrefab, spawnLoc, Quaternion.identity);
+                    Production p = village.GetComponent<Production>();
+                    SpriteRenderer s = village.GetComponent<SpriteRenderer>();
+                    TextMesh t = village.GetComponentInChildren<TextMesh>();
+                    p.ownerID = 1;
+                    // i need to set: village income, sprite, and text number indicating size
+                    t.text = villageSize[i].ToString();
+                    switch (villageType[i]) {
+                        case Production.Resource.Food:
+                            p.income[(int)Production.Resource.Food] = 2 + villageSize[i];
+                            s.sprite = ironImg;
+                            s.sprite = farmImg;
+                            break;
+                        case Production.Resource.Cement:
+                            p.income[(int)Production.Resource.Food] = -villageSize[i];
+                            p.income[(int)Production.Resource.Cement] = villageSize[i];
+                            s.sprite = quarryImg;
+                            break;
+                        case Production.Resource.Lumber:
+                            p.income[(int)Production.Resource.Food] = -villageSize[i];
+                            p.income[(int)Production.Resource.Cement] = 1 + villageSize[i];
+                            s.sprite = lumberImg;
+                            break;
+                        case Production.Resource.Steel:
+                            p.income[(int)Production.Resource.Food] = -villageSize[i];
+                            p.income[(int)Production.Resource.Cement] = 1 + villageSize[i];
+                            s.sprite = ironImg;
+                            break;
+                        default:
+                            break;
+                    }
+                    if(i == fIndex) {
+                        makeCap(1, new Vector3(topLeft.x , topLeft.y * -1, 0),  village.transform);
+                    }
+                }
+            }
+        }
 
-		// TODO: place capitals and connect them to their starter farms
+        // mirror mirror on the wall, which kingdom be the edgiest of them all?
+        //positionWithinCell = reversedList (positionWithinCell);
+        //villageSize = reversedList (villageSize);
+        //villageType = reversedList (villageType);
 
+        // yes i literally copypasted but i gotta change some shit too
+        for (int row = 0, i = halfSize - 1; row < boardY; ++row) {
+            for (int col = row + 2; col < boardX; ++col, --i) {
+                if (villageSize[i] > 0) {
+                    //do math with row/col num to get screen coords
+                    Vector3 spawnLoc = Vector3.Reflect(
+                        new Vector3(cellSize.x * col - positionWithinCell[i].x - Mathf.Abs(topLeft.x),
+                            (cellSize.y * row - positionWithinCell[i].y - Mathf.Abs(topLeft.y)), 0.0f),
+                        Vector3.up);
+                    //instantiate based on production type
+                    GameObject village = Instantiate(villagePrefab, spawnLoc, Quaternion.identity);
+                    Production p = village.GetComponent<Production>();
+                    SpriteRenderer s = GetComponent<SpriteRenderer>();
+                    TextMesh t = GetComponentInChildren<TextMesh>();
+                    p.ownerID = 2;
+                    // i need to set: village income, sprite, and text number indicating size
+                    t.text = villageSize[i].ToString();
+                    switch (villageType[i]) {
+                        case Production.Resource.Food:
+                            p.income[(int)Production.Resource.Food] = 2 + villageSize[i];
+                            s.sprite = farmImg;
+                            break;
+                        case Production.Resource.Cement:
+                            p.income[(int)Production.Resource.Food] = -villageSize[i];
+                            p.income[(int)Production.Resource.Cement] = villageSize[i];
+                            s.sprite = quarryImg;
+                            break;
+                        case Production.Resource.Lumber:
+                            p.income[(int)Production.Resource.Food] = -villageSize[i];
+                            p.income[(int)Production.Resource.Cement] = 1 + villageSize[i];
+                            s.sprite = lumberImg;
+                            break;
+                        case Production.Resource.Steel:
+                            p.income[(int)Production.Resource.Food] = -villageSize[i];
+                            p.income[(int)Production.Resource.Cement] = 1 + villageSize[i];
+                            s.sprite = ironImg;
+                            break;
+                        default:
+                            break;
+                    }
+                    if(i == fIndex) {
+                        makeCap(2, new Vector3(topLeft.x * -1, topLeft.y , 0),  village.transform);
+                    }
+                }
+            }
+        }
+    }
+
+    private void makeCap(int player, Vector3 pos, Transform farm) {
+        /*creates a captial, at the given position, for the given player, with the given starting farm*/
+        GameObject cap = GameObject.Instantiate(capitalPrefab);
+        cap.transform.position = pos;
+        Production p = cap.GetComponent<Production>();
+        p.income[(int)Production.Resource.Food] = -1;
+        p.income[(int)Production.Resource.Cement] = 7;
+        p.income[(int)Production.Resource.Steel] = 5;
+        p.income[(int)Production.Resource.Lumber] = 5;
+        p.ownerID = player;
+        cap.GetComponent<Spawner>().Spawn(Spawner.Objs.ROAD, cap.transform, farm);
 	}
 
 	/*
