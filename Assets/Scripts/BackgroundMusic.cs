29df28448f573600816bf382087d4d6352d73445
Rikerd <cursedmarkedsasuke@gmail.com>
2017-04-24 00:00:15 -0700

    Main Scene

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+    }
+
+    private void Awake()
+    {
+        gameObject.GetComponent<AudioSource>().Play();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //Debug.Log(GetComponent<AudioSource>().clip);
+        //GetComponent<AudioSource>().Play();
+    }
+}
